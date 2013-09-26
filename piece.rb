6fbac55a83e791a7ebc3ec76929a63d25c9e3b47
Jaydeep <jdsingh94@gmail.com>
2013-09-26 11:49:22 -0700

    Get_Move_Dirs; Pos_Within_Range; Possible_Slide_Moves

--- piece.rb
+++ piece.rb
@@ -1,3 +1,4 @@
+require 'debugger'
 require './board.rb'
 
 class Piece
@@ -13,12 +14,43 @@ class Piece
     color.to_s[0].upcase #return "W" or "R"
   end
 
-  def slide_moves
-    #moves one place
+  def get_move_dirs
+  y_dir = (color == :black) ? -1 : 1 #return y_dir based on color
+  [ [ 1, y_dir], [-1, y_dir] ]
   end
 
-  def jump_moves
-    #jumps and kills
+  def pos_within_range?(pos)
+    pos.all? { |coord| coord.between?(0,7) }
+  end
+
+  def possible_slide_moves(current_pos)
+    debugger
+    poss_slide_moves = []
+    x, y = current_pos
+    #moves one place
+    #based on direction, can move towards one of two diagonals
+
+    #1. get diagonals
+    move_dirs = get_move_dirs
+
+    move_dirs.each do |move_dir|
+      dx, dy = move_dir
+      #2. add diagonals to current_pos
+      new_x = x + dx
+      new_y = y + dy
+      new_pos = [new_x, new_y]
+      #3. ensure that new_pos is within range
+      pos_within_range?(new_pos)
+
+      #4. ensure that spot is nil
+      poss_slide_moves << new_pos if @board[new_pos].nil?
+    end
+    #5. return the possible slide movees
+    poss_slide_moves
+  end
+
+  def possible_jump_moves
+    #find opportunities to kill
   end
 
   # def perform_slide
